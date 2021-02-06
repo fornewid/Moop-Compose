c859b5c4c12ba500bd0dd674916fbfeb6325eba5
SOUP <soupyong@gmail.com>
2021-02-06 21:45:10 +0900

    Initialize Project

---
+++ app/src/main/java/soup/movie/compose/MainActivity.kt
@@ -0,0 +1,57 @@
+package soup.movie.compose
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.preferredSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.chrisbanes.accompanist.glide.GlideImage
+import soup.movie.compose.ui.theme.MoopComposeTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MoopComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    LazyColumn {
+        item {
+            Text(text = "Hello $name!")
+        }
+        item {
+            Card {
+                GlideImage(
+                    data = "https://picsum.photos/300/300",
+                    contentDescription = null,
+                    modifier = Modifier.preferredSize(128.dp)
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MoopComposeTheme {
+        Greeting("Android")
+    }
+}
